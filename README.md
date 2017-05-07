e7bc540f43247165918333780376d00bcb66f4d3
worldofth <worldofth@hotmail.co.uk>
2017-05-07 15:40:20 +0100

    made responsive, setup website files for upload, updated readme

--- README.md
+++ README.md
@@ -1,53 +1,16 @@
-# thoughts
+# Tom Hopkins website
 
-don't think about colours for the time being, work on shaed of grey and add colour in later
+For the design i drew inspiration from:
+- [Nier:Automata Options UI](https://www.pcinvasion.com/wp-content/uploads/2017/03/NieR-Automata-visuals-1.jpg)
+- [Elementalist Lux Skin Overview](http://na.leagueoflegends.com/en/featured/skins/elementalist-lux)
+- [Nekomonogatari Opening animation](https://vimeo.com/109421461)
 
-decoration ideas to emphasise and guide people through content
-this will also animate based on mouse position/acelarometer
-look at elementalist lux site for more ideas for decoration
+With this design i was able to play around with different techniques i don't touch very often, like for instance text shadow, Rotated text, background textures, decorative elements to draw the eye and new tech like grid and object fit.
 
-o- big circle and line pointing towards title with animation
-[] box with lines crossing around big bitsa of content
-
-
-
-## header/intro
-nav uses grimgar box animation
-my names, im not sure, possibly something from shiro nekomonogatari intro
-	thinking like reverse shading, so cutout my name
-	thinking also animate in box and then cut out the label things in shiro
-
-## about
-
-
-## skills
-know i want dark background
-want some sort of progress bar thing for skills
-	might want to think if there is a commong shape i can use to denote the out of 5
-	think like in shiro mono there they have lots of labels or cricles animate in
-
-want to have a list of words that relate to each skill names
-want these to look secondary and not pronouced, might flash them up during intial page view to let people know they exist
-
-## portfolio
-white background to show distinct change from previous section
-grid of websites i've built
-think small blurb per site with some thoughts
-have them in date order
-when click on the image they animate out into full screen with small blur
-look at codrops for ideas, know they have some blog grid view that animates from the indivdual grid items
-
-## playground (later)
-similar to portoflio, but showing off some of my experients
-this can be a later edition
-but will probs some together through the creation of this
-can also have partial discussion on functional programming 
-css architectures etc.
-can use codepen.io for posts probs and playground stuff
-
-## links
-
-
-### ideas
-look at http://www.palettable.io/4889D9
-think about picking some colours then finding stuff based on colour thoery like complementry etc.
+## todo
+- test and fix on different browsers
+- add links and warnings on demo pages
+- select/add colours
+- add in more animation
+- add in smooth scroll animation
+- add more projects
--- gulpfile.js
+++ gulpfile.js
@@ -18,50 +18,50 @@ function resolvePath(pathInput) {
 // JS copy
 gulp.task('pl-copy:js', function(){
   return gulp.src('**/*.js', {cwd: resolvePath(paths().source.js)} )
-    .pipe(gulp.dest(resolvePath(paths().public.js)));
+	.pipe(gulp.dest(resolvePath(paths().public.js)));
 });
 
 // Images copy
 gulp.task('pl-copy:img', function(){
   return gulp.src('**/*.*',{cwd: resolvePath(paths().source.images)} )
-    .pipe(gulp.dest(resolvePath(paths().public.images)));
+	.pipe(gulp.dest(resolvePath(paths().public.images)));
 });
 
 // Favicon copy
 gulp.task('pl-copy:favicon', function(){
   return gulp.src('favicon.ico', {cwd: resolvePath(paths().source.root)} )
-    .pipe(gulp.dest(resolvePath(paths().public.root)));
+	.pipe(gulp.dest(resolvePath(paths().public.root)));
 });
 
 // Fonts copy
 gulp.task('pl-copy:font', function(){
   return gulp.src('*', {cwd: resolvePath(paths().source.fonts)})
-    .pipe(gulp.dest(resolvePath(paths().public.fonts)));
+	.pipe(gulp.dest(resolvePath(paths().public.fonts)));
 });
 
 // CSS Copy
 gulp.task('pl-copy:css', function(){
   return gulp.src([resolvePath(paths().source.css) + '/*.css', resolvePath(paths().source.css) + '/*.css.map'])
-    .pipe(gulp.dest(resolvePath(paths().public.css)))
-    .pipe(browserSync.stream());
+	.pipe(gulp.dest(resolvePath(paths().public.css)))
+	.pipe(browserSync.stream());
 });
 
 // Styleguide Copy everything but css
 gulp.task('pl-copy:styleguide', function(){
   return gulp.src(resolvePath(paths().source.styleguide) + '/**/!(*.css)')
-    .pipe(gulp.dest(resolvePath(paths().public.root)))
-    .pipe(browserSync.stream());
+	.pipe(gulp.dest(resolvePath(paths().public.root)))
+	.pipe(browserSync.stream());
 });
 
 // Styleguide Copy and flatten css
 gulp.task('pl-copy:styleguide-css', function(){
   return gulp.src(resolvePath(paths().source.styleguide) + '/**/*.css')
-    .pipe(gulp.dest(function(file){
-      //flatten anything inside the styleguide into a single output dir per http://stackoverflow.com/a/34317320/1790362
-      file.path = path.join(file.base, path.basename(file.path));
-      return resolvePath(path.join(paths().public.styleguide, '/css'));
-    }))
-    .pipe(browserSync.stream());
+	.pipe(gulp.dest(function(file){
+	  //flatten anything inside the styleguide into a single output dir per http://stackoverflow.com/a/34317320/1790362
+	  file.path = path.join(file.base, path.basename(file.path));
+	  return resolvePath(path.join(paths().public.styleguide, '/css'));
+	}))
+	.pipe(browserSync.stream());
 });
 
 /******************************************************
@@ -85,16 +85,16 @@ function build(done) {
 
 gulp.task('pl-assets', gulp.series(
   gulp.parallel(
-    'pl-copy:js',
-    'pl-copy:img',
-    'pl-copy:favicon',
-    'pl-copy:font',
-    'pl-copy:css',
-    'pl-copy:styleguide',
-    'pl-copy:styleguide-css'
+	'pl-copy:js',
+	'pl-copy:img',
+	'pl-copy:favicon',
+	'pl-copy:font',
+	'pl-copy:css',
+	'pl-copy:styleguide',
+	'pl-copy:styleguide-css'
   ),
   function(done){
-    done();
+	done();
   })
 );
 
@@ -141,7 +141,7 @@ function getSupportedTemplateExtensions() {
 }
 function getTemplateWatches() {
   return getSupportedTemplateExtensions().map(function (dotExtension) {
-    return resolvePath(paths().source.patterns) + '/**/*' + dotExtension;
+	return resolvePath(paths().source.patterns) + '/**/*' + dotExtension;
   });
 }
 
@@ -158,13 +158,13 @@ function watch() {
   gulp.watch(resolvePath(paths().source.styleguide) + '/**/*.*', { awaitWriteFinish: true }).on('change', gulp.series('pl-copy:styleguide', 'pl-copy:styleguide-css', reloadCSS));
 
   var patternWatches = [
-    resolvePath(paths().source.patterns) + '/**/*.json',
-    resolvePath(paths().source.patterns) + '/**/*.md',
-    resolvePath(paths().source.data) + '/*.json',
-    resolvePath(paths().source.fonts) + '/*',
-    resolvePath(paths().source.images) + '/*',
-    resolvePath(paths().source.meta) + '/*',
-    resolvePath(paths().source.annotations) + '/*'
+	resolvePath(paths().source.patterns) + '/**/*.json',
+	resolvePath(paths().source.patterns) + '/**/*.md',
+	resolvePath(paths().source.data) + '/*.json',
+	resolvePath(paths().source.fonts) + '/*',
+	resolvePath(paths().source.images) + '/*',
+	resolvePath(paths().source.meta) + '/*',
+	resolvePath(paths().source.annotations) + '/*'
   ].concat(getTemplateWatches());
 
   console.log(patternWatches);
@@ -176,34 +176,34 @@ function watch() {
 
 gulp.task('patternlab:connect', gulp.series(function(done) {
   browserSync.init({
-    server: {
-      baseDir: resolvePath(paths().public.root)
-    },
-    snippetOptions: {
-      // Ignore all HTML files within the templates folder
-      blacklist: ['/index.html', '/', '/?*']
-    },
-    notify: {
-      styles: [
-        'display: none',
-        'padding: 15px',
-        'font-family: sans-serif',
-        'position: fixed',
-        'font-size: 1em',
-        'z-index: 9999',
-        'bottom: 0px',
-        'right: 0px',
-        'border-top-left-radius: 5px',
-        'background-color: #1B2032',
-        'opacity: 0.4',
-        'margin: 0',
-        'color: white',
-        'text-align: center'
-      ]
-    }
+	server: {
+	  baseDir: resolvePath(paths().public.root)
+	},
+	snippetOptions: {
+	  // Ignore all HTML files within the templates folder
+	  blacklist: ['/index.html', '/', '/?*']
+	},
+	notify: {
+	  styles: [
+		'display: none',
+		'padding: 15px',
+		'font-family: sans-serif',
+		'position: fixed',
+		'font-size: 1em',
+		'z-index: 9999',
+		'bottom: 0px',
+		'right: 0px',
+		'border-top-left-radius: 5px',
+		'background-color: #1B2032',
+		'opacity: 0.4',
+		'margin: 0',
+		'color: white',
+		'text-align: center'
+	  ]
+	}
   }, function(){
-    console.log('PATTERN LAB NODE WATCHING FOR CHANGES');
-    done();
+	console.log('PATTERN LAB NODE WATCHING FOR CHANGES');
+	done();
   });
 }));
 
@@ -211,22 +211,24 @@ gulp.task('patternlab:connect', gulp.series(function(done) {
  * SASS
 ******************************************************/
 var sass = require('gulp-sass'),
-  sourcemaps = require('gulp-sourcemaps');
+  sourcemaps = require('gulp-sourcemaps'),
+  autoprefixer = require('gulp-autoprefixer');
 
 function buildSass(){
   return gulp.src(resolvePath(paths().source.css) + '/main.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-      //outputStyle: 'compressed'
-    }).on('error', console.log))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(resolvePath(paths().source.css)));
+	.pipe(sourcemaps.init())
+	.pipe(sass({
+		outputStyle: 'compressed'
+	})
+	.on('error', console.log))
+	.pipe(sourcemaps.write('.'))
+	.pipe(gulp.dest(resolvePath(paths().source.css)));
 }
 
 function watchSass(){
   gulp.watch(resolvePath(paths().source.css) + '/**/*.scss', { awaitWriteFinish: true })
   .on('change',  gulp.series(buildSass, function(path){
-    console.log('File ' + path + ' was changed');
+	console.log('File ' + path + ' was changed');
   }));
 }
 
