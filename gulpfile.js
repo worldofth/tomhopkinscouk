decf7775b44116922771f84fbb560cdbab03e61b
Tom <tom@servondesign.co.uk>
2017-05-08 14:21:39 +0100

    got prefixer working, though it didn't fix IE 11. probs do a bit php for checking and pop up a big message to tell people not to use IE.

--- gulpfile.js
+++ gulpfile.js
@@ -212,18 +212,38 @@ gulp.task('patternlab:connect', gulp.series(function(done) {
 ******************************************************/
 var sass = require('gulp-sass'),
   sourcemaps = require('gulp-sourcemaps'),
-  autoprefixer = require('gulp-autoprefixer');
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  cssnano = require('gulp-cssnano');
 
-function buildSass(){
-  return gulp.src(resolvePath(paths().source.css) + '/main.scss')
-	.pipe(sourcemaps.init())
-	.pipe(sass({
-		outputStyle: 'compressed'
-	})
-	.on('error', console.log))
-	.pipe(sourcemaps.write('.'))
-	.pipe(gulp.dest(resolvePath(paths().source.css)));
+function compileSass(){
+	return gulp.src(resolvePath(paths().source.css) + '/main.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass())
+		.on('error', console.log)
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(resolvePath(paths().source.css)));
 }
+
+function prefixCss(){
+	return gulp.src(resolvePath(paths().source.css) + '/main.css')
+		.pipe(sourcemaps.init({loadMaps: true}))
+		.pipe(postcss( [autoprefixer({browsers: ['last 2 versions', 'IE 11']})] ))
+		.on('error', console.log)
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(resolvePath(paths().source.css)));
+}
+
+function minifyCss(){
+	return gulp.src(resolvePath(paths().source.css) + '/main.css')
+		.pipe(sourcemaps.init({loadMaps: true}))
+		.pipe(cssnano())
+		.on('error', console.log)
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(resolvePath(paths().source.css)));
+}
+
+var buildSass = gulp.series(compileSass, prefixCss, minifyCss);
 
 function watchSass(){
   gulp.watch(resolvePath(paths().source.css) + '/**/*.scss', { awaitWriteFinish: true })
